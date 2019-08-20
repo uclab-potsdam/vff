2c7b35cbc4c383180d7d8782802440de39bcd020
Jonas <jonasparnow@gmail.com>
2019-08-20 12:15:52 +0200

    Favicon added

--- assets/js/head.js
+++ assets/js/head.js
@@ -3,7 +3,8 @@
 module.exports = {
   links: function (config) {
     return [
-      { rel: 'license', href: config.page.imprint }
+      { rel: 'license', href: config.page.imprint },
+      { rel: 'manifest', href: config.page.url + '/site.webmanifest' }
     ]
   }
 }
