27c01e3f039147308053dca197f82a6d5acc2025
Jonas <jonasparnow@gmail.com>
2019-08-20 12:46:19 +0200

    New favicons

--- assets/js/favicon.js
+++ assets/js/favicon.js
@@ -5,7 +5,9 @@ module.exports = {
     return [
       { rel: 'apple-touch-icon', sizes: '180x180', href: base + '/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: base + '/favicon-32x32.png' },
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: base + '/favicon-16x16.png' }
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: base + '/favicon-16x16.png' },
+      { rel: 'manifest', href: base + '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#11ed94', href: base + '/safari-pinned-tab.svg' },
     ]
   },
   msapplication: function (base) {
